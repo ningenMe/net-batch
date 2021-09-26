25b857dd99cd3f1086a0d1bd2f10fa08097c2603
ningenMe <amohkuru@gmail.com>
2021-09-26 20:42:22 +0900

    [fix]

--- src/main/java/ningenme/net/batch/infrastructure/mysql/mapper/RelationCreatorComicMysqlMapper.java
+++ src/main/java/ningenme/net/batch/infrastructure/mysql/mapper/RelationCreatorComicMysqlMapper.java
@@ -12,9 +12,12 @@ public interface RelationCreatorComicMysqlMapper {
 
     @Insert(
         "<script>" +
-        "INSERT IGNORE INTO relation_creators_comics (creator_name, url) VALUES " +
+        "INSERT IGNORE INTO relation_creators_comics (creator_id, comic_id) VALUES " +
         "<foreach item='item' collection='relationCreatorComicMysqlDtoList' open='' separator=',' close=''>" +
-        "(#{item.creatorName},#{item.url}) " +
+        "(" +
+        " (SELECT creator_id FROM creators WHERE creator_name = #{item.creatorName}), " +
+        " (SELECT comic_id FROM comics WHERE url = #{item.url})" +
+        ") " +
         "</foreach>" +
         "</script>"
     )
